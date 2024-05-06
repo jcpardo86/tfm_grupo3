ac8911b70a74833ab3a0bcbc4e3692a039226ff5
Marcos <marcos.canal779@comunidadunir.net>
2024-05-06 20:35:55 +0200

    Revert "Merge pull request #1 from jcpardo86/develop"
    This reverts commit 09856aa86c57afb5f1e6f4ccff1638af8ad716ae, reversing
    changes made to 8ab96cfb462a5e3e94ac9e32ae031d081881a0df.

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,7 +1,3 @@
 import { Routes } from '@angular/router';
-import { HomeComponent } from './pages/home/home.component';
 
-export const routes: Routes = [
-	{ path: "", redirectTo: "home", pathMatch: "full" },
-	{ path: "home", component: HomeComponent }
-];
+export const routes: Routes = [];
