9b86aac63fed52e013baac47a98506b835b8997c
Lara Martin <lara.martin.lagares@gmail.com>
2024-05-18 12:22:48 +0200

    Definición de contenido html, estilo css y funcionalidad js

--- src/app/components/spent-card/spent-card.component.ts
+++ src/app/components/spent-card/spent-card.component.ts
@@ -1,4 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, Input, inject } from '@angular/core';
+import { ISpent } from '../../interfaces/ispent.interface';
+import { UsersService } from '../../services/users.service';
+import { IUser } from '../../interfaces/iuser.interface';
 
 @Component({
   selector: 'app-spent-card',
@@ -9,4 +12,30 @@ import { Component } from '@angular/core';
 })
 export class SpentCardComponent {
 
+  @Input() mySpent!: ISpent;
+
+  @Input() numSpent!: number;
+
+  myUser: IUser = {
+    idUsuario: 0,
+    nombre: "",
+    apellidos: "",
+    email: "",
+    password: ""
+  };
+
+  userService = inject(UsersService);
+
+  async ngOnInit(): Promise<any>{
+
+    try{
+      const response = await this.userService.getUserById(this.mySpent.idUsuario);
+      this.myUser = response;
+
+    } catch(err){
+      console.log(err)
+    }
+
+  }
 }
+
