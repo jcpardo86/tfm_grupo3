35589a8fe620f97d0f0115bd01c14c9c414e8a1d
Lara Martin <lara.martin.lagares@gmail.com>
2024-05-18 12:00:59 +0200

    Creación de servicios de grupos, usuarios, gastos y chat, para interactuar con la API externa

---
+++ src/app/services/users.service.ts
@@ -0,0 +1,28 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable, inject } from '@angular/core';
+import { lastValueFrom } from 'rxjs';
+import { IUser } from '../interfaces/iuser.interface';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class UsersService {
+
+  private httpClient = inject(HttpClient);
+  private baseUrl : string = 'http://localhost:3000/api/users'
+
+ 
+
+  registertUser(user: IUser): Promise<IUser>{
+    return lastValueFrom(this.httpClient.post<IUser>(`${this.baseUrl}/register`, user));
+  };
+
+  loginUser(user: any): Promise<any>{
+    return lastValueFrom(this.httpClient.post<IUser>(`${this.baseUrl}/login`, user));
+  };
+
+  getUserById(idUser: number): Promise<IUser> {
+    return lastValueFrom(this.httpClient.get<IUser>(`${this.baseUrl}/${idUser}`));
+  };
+  
+}
