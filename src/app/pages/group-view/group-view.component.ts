9e04f62afc2e6498549790cd5113f9f400350a93
Lara Martin <lara.martin.lagares@gmail.com>
2024-05-18 12:07:19 +0200

    Creación de page groupView e importación del componente en el fichero de rutas

---
+++ src/app/pages/group-view/group-view.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-group-view',
+  standalone: true,
+  imports: [],
+  templateUrl: './group-view.component.html',
+  styleUrl: './group-view.component.css'
+})
+export class GroupViewComponent {
+
+}
