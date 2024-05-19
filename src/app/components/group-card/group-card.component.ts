3ef40670715281f47fb41c40495b706686ce554f
Lara Martin <lara.martin.lagares@gmail.com>
2024-05-19 22:34:56 +0200

    Corrección en js y html  de componente groupCard

--- src/app/components/group-card/group-card.component.ts
+++ src/app/components/group-card/group-card.component.ts
@@ -1,6 +1,10 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, inject } from '@angular/core';
 import { IGroupUser } from '../../interfaces/igroup-user.interface';
-import { RouterLink } from '@angular/router';
+import { Router, RouterLink } from '@angular/router';
+import { GroupsService } from '../../services/groups.service';
+import { IGroup } from '../../interfaces/igroup.interface';
+import { IUser } from '../../interfaces/iuser.interface';
+import { SpentsService } from '../../services/spents.service';
 
 @Component({
   selector: 'app-group-card',
@@ -13,4 +17,54 @@ export class GroupCardComponent {
 
   @Input() myGroup!: IGroupUser;
 
+  group!: IGroup;
+
+  users!: IUser[];
+
+  total: number = 0;
+
+  groupService = inject(GroupsService);
+
+  spentService = inject(SpentsService);
+
+  router = inject(Router);
+
+  async ngOnInit(): Promise<any> {
+
+    try {
+      const response = await this.groupService.getGroupById(this.myGroup.idGrupo);
+      if(response!=undefined) {
+        this.group = response;
+      } else {
+        console.log('No se pueden obtener los datos del grupo')
+      }
+
+    } catch(err) {
+        this.router.navigate(['/error']);
+    }
+
+    try {
+      const response = await this.groupService.getUsersByGroup(this.myGroup.idGrupo);
+      if(response!=undefined) {
+        this.users = response;
+      } else {
+        console.log('No se pueden obtener los usuarios del grupo')
+      }
+
+    } catch(err) {
+        this.router.navigate(['/error']);
+    }
+
+    try {
+      const response = await this.spentService.getTotalSpentByGroup(this.myGroup.idGrupo);
+      if(response!=undefined) {
+        this.total = response.total_importe;
+      } else {
+        console.log('No se pueden obtener el total de gastos del grupo')
+      }
+
+    } catch(err) {
+        this.router.navigate(['/error']);
+    }
+  }
 }
