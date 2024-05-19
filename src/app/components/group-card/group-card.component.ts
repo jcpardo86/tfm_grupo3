7889b9d8061a1b29f1b8b7d738e6c0facdf5cc9d
Lara Martin <lara.martin.lagares@gmail.com>
2024-05-19 21:56:25 +0200

    Creación de componente groupCard

---
+++ src/app/components/group-card/group-card.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-group-card',
+  standalone: true,
+  imports: [],
+  templateUrl: './group-card.component.html',
+  styleUrl: './group-card.component.css'
+})
+export class GroupCardComponent {
+
+}
