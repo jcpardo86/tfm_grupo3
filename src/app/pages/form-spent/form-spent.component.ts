fcbfaa3e8dd6bfe7dc72000b31770588ff4efc58
jgamonal <josue.gamonal@gmail.com>
2024-06-18 19:04:45 +0200

    error de git
    creo que así se resuelve

--- src/app/pages/form-spent/form-spent.component.ts
+++ src/app/pages/form-spent/form-spent.component.ts
@@ -13,33 +13,32 @@ import { FooterComponent } from '../../components/footer/footer.component';
 import { NavbarComponent } from '../../components/navbar/navbar.component';
 
 @Component({
-  selector: 'app-form-spent',
-  standalone: true,
-  imports: [FooterComponent, ReactiveFormsModule,NavbarComponent],
-  templateUrl: './form-spent.component.html',
-  styleUrl: './form-spent.component.css'
+	selector: 'app-form-spent',
+	standalone: true,
+	imports: [FooterComponent, ReactiveFormsModule, NavbarComponent],
+	templateUrl: './form-spent.component.html',
+	styleUrl: './form-spent.component.css'
 })
 export class FormSpentComponent {
 
-  //Inyección de ActivatedRoute para obtener params de ruta
-  activatedRouter = inject(ActivatedRoute);
-  //Inyección de Router para redirecciones
-  router = inject(Router);
+	//Inyección de ActivatedRoute para obtener params de ruta
+	activatedRouter = inject(ActivatedRoute);
+	//Inyección de Router para redirecciones
+	router = inject(Router);
 
-  //Inyección de servicios SpentsService, DebtsService y GroupsService, para gestión de gastos, deudas y grupos
-  spentService = inject(SpentsService);
-  debtService = inject(DebtsService)
-  groupService = inject(GroupsService);
+	//Inyección de servicios SpentsService, DebtsService y GroupsService, para gestión de gastos, deudas y grupos
+	spentService = inject(SpentsService);
+	debtService = inject(DebtsService)
+	groupService = inject(GroupsService);
 
-  tipo: string = 'AÑADIR';
-  boton: string = 'Guardar';
+	tipo: string = 'AÑADIR';
+	boton: string = 'Guardar';
 
-  modelForm: FormGroup;
+	modelForm: FormGroup;
 
-  users: IUser[] = []; //Objeto para almacenar el listado de usuarios del grupo
- 
-  id_group!: number;  //Propiedad para almacenar el id del grupo
+	users: IUser[] = []; //Objeto para almacenar el listado de usuarios del grupo
 
+<<<<<<< Updated upstream
   image: String = "";  //Propiedad para almacenar la imagen del grupo
   
   constructor() {
@@ -60,64 +59,91 @@ export class FormSpentComponent {
         Validators.required
       ]), 
     }, [])
+=======
+	id_group!: number;  //Propiedad para almacenar el id del grupo
+>>>>>>> Stashed changes
 
-  }
+	image: String = "";  //Propiedad para almacenar la imagen del grupo
 
-  async getDataForm(): Promise<any> {
+	constructor() {
+		this.modelForm = new FormGroup({
+			idGasto: new FormControl('', []),
+			idGrupo: new FormControl('', []),
+			idUsuario: new FormControl('', [
+				Validators.required,
+			]),
+			descripcion: new FormControl('', [
+				Validators.required
+			]),
+			importe: new FormControl('', [
+				Validators.required,
+				Validators.pattern(/^[0-9]+([.][0-9]+)?$/)
+			]),
+			fecha: new FormControl('', [
+				Validators.required
+			]),
+		}, [])
 
-    if(this.modelForm.value.idGasto) {
-      // Formulario de actualización:
-      try {
-        //Solicitamos actualización del gasto en BBDD
-        const response_1 = await this.spentService.updateSpent(this.modelForm.value);
-        //Solicitamos actualización del saldo de todos los usuarios del grupo en BBDD
-        for(let user of this.users) {
-          const response_2 = await this.spentService.updateSaldo({idGrupo: parseInt(this.modelForm.value.idGrupo), idUsuario: user.idUsuario});
-        }
-        //Solicitamos actualización de deudas del grupo en BBDD
-        const response_3 = await this.debtService.updateDebtsByGroup(this.modelForm.value);
+	}
 
-        //Mensaje para informar al usuario de que el gasto ha sido actualizado
-        Swal.fire({
-          text: "El gasto ha sido actualizado",
-          icon: "success",
-          confirmButtonColor: "#FE5F42"
-        });
-        this.router.navigate([`/group/${response_1.idGrupo}`])
+	async getDataForm(): Promise<any> {
 
-      } catch(error) {
-        console.log(error);
-      }
-    } else {
-      //Formulario de registro:
-      this.modelForm.value.idGrupo = this.id_group;
-      try {
-        // Solicitamos inserción de nuevo gasto en BBDD
-        const response_1 = await this.spentService.insertSpent(this.modelForm.value);
-        //Solicitamos actualización del saldo de todos los usuarios del grupo en BBDD
-        for(let user of this.users) {
-          const response_2 = await this.spentService.updateSaldo({idGrupo: parseInt(this.modelForm.value.idGrupo), idUsuario: user.idUsuario}); 
-        }
-        //Solicitamos actualización de deudas del grupo en BBDD
-        const response_3 = await this.debtService.updateDebtsByGroup(this.modelForm.value);
+		if (this.modelForm.value.idGasto) {
+			// Formulario de actualización:
+			try {
+				//Solicitamos actualización del gasto en BBDD
+				const response_1 = await this.spentService.updateSpent(this.modelForm.value);
+				//Solicitamos actualización del saldo de todos los usuarios del grupo en BBDD
+				for (let user of this.users) {
+					const response_2 = await this.spentService.updateSaldo({ idGrupo: parseInt(this.modelForm.value.idGrupo), idUsuario: user.idUsuario });
+				}
+				//Solicitamos actualización de deudas del grupo en BBDD
+				const response_3 = await this.debtService.updateDebtsByGroup(this.modelForm.value);
 
-        //Mensaje para informar al usuario de que el gasto ha sido añadido al grupo
-        Swal.fire({
-          text: "El gasto ha sido añadido al grupo",
-          icon: "success",
-          confirmButtonColor: "#FE5F42"
-        });
-        this.router.navigate([`/group/${this.id_group}`]);
+				//Mensaje para informar al usuario de que el gasto ha sido actualizado
+				Swal.fire({
+					text: "El gasto ha sido actualizado",
+					icon: "success",
+					confirmButtonColor: "#FE5F42"
+				});
+				this.router.navigate([`/group/${response_1.idGrupo}`])
 
-      } catch(error) {
-        console.log(error);
-      }
-    }
-  }
+			} catch (error) {
+				console.log(error);
+			}
+		} else {
+			//Formulario de registro:
+			this.modelForm.value.idGrupo = this.id_group;
+			try {
+				// Solicitamos inserción de nuevo gasto en BBDD
+				const response_1 = await this.spentService.insertSpent(this.modelForm.value);
+				console.log("valor del gasto", this.modelForm.value);
+
+				//Solicitamos actualización del saldo de todos los usuarios del grupo en BBDD
+				for (let user of this.users) {
+					const response_2 = await this.spentService.updateSaldo({ idGrupo: parseInt(this.modelForm.value.idGrupo), idUsuario: user.idUsuario });
+				}
+				//Solicitamos actualización de deudas del grupo en BBDD
+				const response_3 = await this.debtService.updateDebtsByGroup(this.modelForm.value);
+
+				//Mensaje para informar al usuario de que el gasto ha sido añadido al grupo
+				Swal.fire({
+					text: "El gasto ha sido añadido al grupo",
+					icon: "success",
+					confirmButtonColor: "#FE5F42"
+				});
+				this.router.navigate([`/group/${this.id_group}`]);
+
+			} catch (error) {
+				console.log(error);
+			}
+		}
+	}
 
 
-  async ngOnInit() {
+	async ngOnInit() {
 
+<<<<<<< Updated upstream
     this.activatedRouter.params.subscribe(async (params:any) => {
       
       if (params.id_spent) { 
@@ -129,61 +155,48 @@ export class FormSpentComponent {
           this.id_group = response.idGrupo;
           const date = dayjs(response.fecha).format("YYYY-MM-DD");
 		      response.fecha = date;
+=======
+		this.activatedRouter.params.subscribe(async (params: any) => {
+>>>>>>> Stashed changes
 
-          this.modelForm = new FormGroup({
-          idGasto: new FormControl(response.idGasto,[]),
-          idGrupo: new FormControl(response.idGrupo,[]),
-          idUsuario: new FormControl(response.idUsuario,[]),
-          descripcion: new FormControl(response.descripcion,[
-            Validators.required
-          ]),
-          importe: new FormControl(response.importe,[
-            Validators.required
-          ]),
-          fecha: new FormControl(response.fecha,[
-            Validators.required
-          ])
-          }, []);
-        } catch (error) {
-          console.log(error);
-        }
+			if (params.id_spent) {
+				//Formulario actualizar: cumplimentamos los datos del gasto en el formulario
+				this.tipo = 'ACTUALIZAR'
+				this.boton = 'Actualizar'
+				try {
+					const response = await this.spentService.spentById(params.id_spent)
+					this.id_group = response.idGrupo;
+					const date = dayjs(response.fecha).format("YYYY-MM-DD");
+					response.fecha = date;
 
-        try {
-          const response = await this.groupService.getImageGroup(this.id_group);
-          if(response[0]!==undefined) {
-            this.image = (`http://localhost:3000/groupimage/${response[0].imagen}`)
-          }
-        } catch(error) {
-          console.log(error);
-        }
-          
-      } else {
-        //Formulario nuevo registro de gasto
-        this.id_group = params.id_group;
-        this.datosSelect(this.id_group);
-       // Solicitamos la imagen del grupo para mostrar en el formulario
-      try {
-        const response = await this.groupService.getImageGroup(this.id_group);
-        if(response[0]!==undefined) {
-          this.image = (`http://localhost:3000/groupimage/${response[0].imagen}`)
-        }
-      } catch(error) {
-        console.log(error);
-      } 
-    }
-  });
-}
+					this.modelForm = new FormGroup({
+						idGasto: new FormControl(response.idGasto, []),
+						idGrupo: new FormControl(response.idGrupo, []),
+						idUsuario: new FormControl(response.idUsuario, []),
+						descripcion: new FormControl(response.descripcion, [
+							Validators.required
+						]),
+						importe: new FormControl(response.importe, [
+							Validators.required
+						]),
+						fecha: new FormControl(response.fecha, [
+							Validators.required
+						])
+					}, []);
+				} catch (error) {
+					console.log(error);
+				}
 
-  // Método para obtener el listado de usuarios(miembros) de un grupo, para mostrar en las opciones del Select (form de registro) o como información (form de actualización).
-  async datosSelect(idGrupo: number | undefined) {
-    try {
-      const response = await this.groupService.getUsersByGroup(idGrupo);
-      this.users = response;
-    } catch(error) {
-      console.log(error);
-    }
-  }
+				try {
+					const response = await this.groupService.getImageGroup(this.id_group);
+					if (response[0] !== undefined) {
+						this.image = (`http://localhost:3000/groupimage/${response[0].imagen}`)
+					}
+				} catch (error) {
+					console.log(error);
+				}
 
+<<<<<<< Updated upstream
   // Verificación de campos
   checkControl(
     formControlName: string,
@@ -203,5 +216,45 @@ export class FormSpentComponent {
                 : t;
     
   }
+=======
+			} else {
+				//Formulario nuevo registro de gasto
+				this.id_group = params.id_group;
+				this.datosSelect(this.id_group);
+				// Solicitamos la imagen del grupo para mostrar en el formulario
+				try {
+					const response = await this.groupService.getImageGroup(this.id_group);
+					if (response[0] !== undefined) {
+						this.image = (`http://localhost:3000/groupimage/${response[0].imagen}`)
+					}
+				} catch (error) {
+					console.log(error);
+				}
+			}
+		});
+	}
+
+	// Método para obtener el listado de usuarios(miembros) de un grupo, para mostrar en las opciones del Select (form de registro) o como información (form de actualización).
+	async datosSelect(idGrupo: number | undefined) {
+		try {
+			const response = await this.groupService.getUsersByGroup(idGrupo);
+			this.users = response;
+		} catch (error) {
+			console.log(error);
+		}
+	}
+
+	// Verificación de campos
+	checkControl(
+		formControlName: string,
+		validador: string
+	): boolean | undefined {
+		return (
+			this.modelForm.get(formControlName)?.hasError(validador) &&
+			this.modelForm.get(formControlName)?.touched
+		);
+	}
+
+>>>>>>> Stashed changes
 }
 
